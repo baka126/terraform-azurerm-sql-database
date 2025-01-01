961489f2ee7287b679449ac39000e5895a37c3c9
baka126 <bikashyadav126@gmail.com>
2025-01-01 15:06:24 +0545

    added azurerm_postgresql_server resource in postgresql module

--- modules/postgresql/mail.tf
+++ modules/postgresql/mail.tf
@@ -27,55 +27,55 @@ resource "azurerm_postgresql_server" "server" {
     avm_yor_name = "server"
   } /*<box>*/ : replace(k, "avm_", var.tracing_tags_prefix) => v } : {}) /*</box>*/))
 
-  dynamic "threat_detection_policy" {
-    for_each = nonsensitive(var.threat_detection_policy) != null ? ["threat_detection_policy"] : []
+#   dynamic "threat_detection_policy" {
+#     for_each = nonsensitive(var.threat_detection_policy) != null ? ["threat_detection_policy"] : []
 
-    content {
-      disabled_alerts            = var.threat_detection_policy.disabled_alerts
-      email_account_admins       = var.threat_detection_policy.email_account_admins
-      email_addresses            = var.threat_detection_policy.email_addresses
-      enabled                    = var.threat_detection_policy.enabled
-      retention_days             = var.threat_detection_policy.retention_days
-      storage_account_access_key = var.threat_detection_policy.storage_account_access_key
-      storage_endpoint           = var.threat_detection_policy.storage_endpoint
-    }
-  }
-}
+#     content {
+#       disabled_alerts            = var.threat_detection_policy.disabled_alerts
+#       email_account_admins       = var.threat_detection_policy.email_account_admins
+#       email_addresses            = var.threat_detection_policy.email_addresses
+#       enabled                    = var.threat_detection_policy.enabled
+#       retention_days             = var.threat_detection_policy.retention_days
+#       storage_account_access_key = var.threat_detection_policy.storage_account_access_key
+#       storage_endpoint           = var.threat_detection_policy.storage_endpoint
+#     }
+#   }
+# }
 
-resource "azurerm_postgresql_database" "dbs" {
-  count = length(var.db_names)
+# resource "azurerm_postgresql_database" "dbs" {
+#   count = length(var.db_names)
 
-  charset             = var.db_charset
-  collation           = var.db_collation
-  name                = var.db_names[count.index]
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.server.name
-}
+#   charset             = var.db_charset
+#   collation           = var.db_collation
+#   name                = var.db_names[count.index]
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_postgresql_server.server.name
+# }
 
-resource "azurerm_postgresql_firewall_rule" "firewall_rules" {
-  count = length(var.firewall_rules)
+# resource "azurerm_postgresql_firewall_rule" "firewall_rules" {
+#   count = length(var.firewall_rules)
 
-  end_ip_address      = var.firewall_rules[count.index]["end_ip"]
-  name                = format("%s%s", var.firewall_rule_prefix, lookup(var.firewall_rules[count.index], "name", count.index))
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.server.name
-  start_ip_address    = var.firewall_rules[count.index]["start_ip"]
-}
+#   end_ip_address      = var.firewall_rules[count.index]["end_ip"]
+#   name                = format("%s%s", var.firewall_rule_prefix, lookup(var.firewall_rules[count.index], "name", count.index))
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_postgresql_server.server.name
+#   start_ip_address    = var.firewall_rules[count.index]["start_ip"]
+# }
 
-resource "azurerm_postgresql_virtual_network_rule" "vnet_rules" {
-  count = length(var.vnet_rules)
+# resource "azurerm_postgresql_virtual_network_rule" "vnet_rules" {
+#   count = length(var.vnet_rules)
 
-  name                = format("%s%s", var.vnet_rule_name_prefix, lookup(var.vnet_rules[count.index], "name", count.index))
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.server.name
-  subnet_id           = var.vnet_rules[count.index]["subnet_id"]
-}
+#   name                = format("%s%s", var.vnet_rule_name_prefix, lookup(var.vnet_rules[count.index], "name", count.index))
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_postgresql_server.server.name
+#   subnet_id           = var.vnet_rules[count.index]["subnet_id"]
+# }
 
-resource "azurerm_postgresql_configuration" "db_configs" {
-  count = length(keys(var.postgresql_configurations))
+# resource "azurerm_postgresql_configuration" "db_configs" {
+#   count = length(keys(var.postgresql_configurations))
 
-  name                = element(keys(var.postgresql_configurations), count.index)
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.server.name
-  value               = element(values(var.postgresql_configurations), count.index)
-}
+#   name                = element(keys(var.postgresql_configurations), count.index)
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_postgresql_server.server.name
+#   value               = element(values(var.postgresql_configurations), count.index)
+# }
