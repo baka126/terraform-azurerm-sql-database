381ee81a2c3ab6d2d0593336585b1097d69f0600
baka126 <bikashyadav126@gmail.com>
2025-01-06 14:32:15 +0545

    fixed checkov issue replica db doesnot require geo backup

--- examples/postgresql/main.tf
+++ examples/postgresql/main.tf
@@ -20,13 +20,44 @@ resource "azurerm_resource_group" "test" {
 module "postgresql" {
   source = "../../modules/postgresql"
 
-  database_type = "postgresql"
+  database_type       = "postgresql"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
+  sku_name            = "GP_Standard_D2s_v3"
 
-  server_name            = "test-pg-primary-${random_id.rg_name.hex}"
-  administrator_login    = "login"
-  administrator_password = random_password.password.result
+  server_name                   = "test-pg-primary-${random_id.rg_name.hex}"
+  administrator_login           = "login"
+  administrator_password        = random_password.password.result
+  public_network_access_enabled = true
+  db_names                      = ["db1"]
+
+  maintenance_window = [
+    {
+      day_of_week  = 1
+      start_hour   = 0
+      start_minute = 0
+    }
+  ]
+
+  firewall_rules = [
+    {
+      name             = "AllowAll"
+      start_ip_address = "0.0.0.0"
+      end_ip_address   = "0.0.0.0"
+    },
+    {
+      name             = "AllowAll2"
+      start_ip_address = "192.168.1.0"
+      end_ip_address   = "192.168.1.255"
+    }
+  ]
+
+  postgresql_configurations = [
+    {
+      name  = "log_connections"
+      value = "on"
+    }
+  ]
 }
 
 resource "time_sleep" "sleep" {
@@ -37,16 +68,18 @@ resource "time_sleep" "sleep" {
 
 module "postgresql_replica" {
   source = "../../modules/postgresql"
-  
-  database_type = "postgresql"
+
+  database_type       = "postgresql"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
+  sku_name            = "GP_Standard_D2s_v3"
 
-  server_name            = "test-pg-replica-${random_id.rg_name.hex}"
-  administrator_login    = "login"
-  administrator_password = random_password.password.result
+  server_name                  = "test-pg-replica-${random_id.rg_name.hex}"
+  administrator_login          = "login"
+  administrator_password       = random_password.password.result
+  geo_redundant_backup_enabled = false
 
-  create_mode               = "Replica"
-  source_server_id          = module.postgresql.server_id
-  #depends_on = [time_sleep.sleep]
+  create_mode      = "Replica"
+  source_server_id = module.postgresql.server_id
+  depends_on       = [time_sleep.sleep]
 }
--- examples/postgresql/provider.tf
+++ examples/postgresql/provider.tf
@@ -1,4 +1,4 @@
 provider "azurerm" {
-  features  {}
+  features {}
   subscription_id = "e2418e71-b878-491f-8211-0c18b34b9809"
 }
