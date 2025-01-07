82a72db9245bfb9ea428d00f74238ef34471759b
baka126 <bikashyadav126@gmail.com>
2025-01-07 10:03:00 +0545

    removed unused variables and fixed firewall rule name

--- examples/postgresql/outputs.tf
+++ examples/postgresql/outputs.tf
@@ -1,6 +1,6 @@
-# output "test_postgresql_replica_server_id" {
-#   value = module.postgresql_replica.server_id
-# }
+output "test_postgresql_replica_server_id" {
+  value = module.postgresql_replica.server_id
+}
 
 output "test_postgresql_server_id" {
   value = module.postgresql.server_id
